91c3b010e216f35a2b15def39b646da04a989528
Sean Fridman <fridman@mail.sfsu.edu>
2012-09-20 20:27:34 -0700

    Nit picked

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   dict: 'assets/dicts/wordlist.txt',
   encoding: 'ascii',
-  delimiter: '\r\n' // Should be Unicode equivalent of delimiter. Not perfect, but whatev.
+  delimiter: '\r\n' // Should be Javascript string equivalent of delimiter. Not perfect, but whatev.
 };
